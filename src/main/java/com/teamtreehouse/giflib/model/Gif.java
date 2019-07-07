553fbac9e1ca41ddcf4861b3a5df89ab6ad6aab5
Chloe Nazaruk <chloe@nazdevelopment.com>
2019-07-07 18:37:56 +0100

    first commit

--- src/main/java/com/teamtreehouse/giflib/model/Gif.java
+++ src/main/java/com/teamtreehouse/giflib/model/Gif.java
@@ -1,4 +1,59 @@
 package com.teamtreehouse.giflib.model;
 
+import java.time.LocalDate;
+
 public class Gif {
+    private String name;
+    private int categoryId;
+    private LocalDate dateUploaded;
+    private String username;
+    private boolean favourite;
+
+    public Gif(String name, int categoryId, LocalDate dateUploaded, String username, boolean favourite) {
+        this.name = name;
+        this.categoryId = categoryId;
+        this.dateUploaded = dateUploaded;
+        this.username = username;
+        this.favourite = favourite;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public int getCategoryId() {
+        return categoryId;
+    }
+
+    public void setCategoryId(int categoryId) {
+        this.categoryId = categoryId;
+    }
+
+    public LocalDate getDateUploaded() {
+        return dateUploaded;
+    }
+
+    public void setDateUploaded(LocalDate dateUploaded) {
+        this.dateUploaded = dateUploaded;
+    }
+
+    public String getUsername() {
+        return username;
+    }
+
+    public void setUsername(String username) {
+        this.username = username;
+    }
+
+    public boolean isFavourite() {
+        return favourite;
+    }
+
+    public void setFavourite(boolean favourite) {
+        this.favourite = favourite;
+    }
 }
